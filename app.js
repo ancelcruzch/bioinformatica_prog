8d51b335373851b2e20348a4874f41d901fffe69
Alain Cruz <acruzch@Alains-MacBook-Air.local>
2024-07-01 23:43:38 -0500

    index app file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 const file1Route = require('./routes/lab_01');
@@ -22,7 +23,4 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
